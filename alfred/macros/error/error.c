c0611bbb2ddd3a191c82dee37ee96b871bb2573c
Dimitri Wyzlic <dimitriwyzlic@gmail.com>
2017-06-26 18:06:23 +0200

    [NORME] error
    Signed-off-by: Dimitri Wyzlic <dimitriwyzlic@gmail.com>

--- alfred/macros/error/error.c
+++ alfred/macros/error/error.c
@@ -5,7 +5,7 @@
 ** Login   <dimitri1.wyzlic@epitech.eu>
 **
 ** Started on  Fri Jun 23 15:35:05 2017 Dimitri Wyzlic
-** Last update Fri Jun 23 15:35:05 2017 Dimitri Wyzlic
+** Last update Mon Jun 26 18:04:42 2017 Doom
 */
 
 #include "error.h"
@@ -16,7 +16,8 @@ char	*get_error(void)
   return (strerror(errnum));
 }
 
-void	*__RAISE__(const char *msg, const char *file, unsigned int l, const char *f)
+void	*__RAISE__(const char *msg, const char *file, unsigned int l,
+		   const char *f)
 {
   fprintf(__NORME__, file, l ,f , msg, get_error());
   exit(1);
